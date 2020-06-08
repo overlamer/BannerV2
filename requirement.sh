235a001dbe3a9959f351da0a0a9c4211cbdc842e
overlamer <65023172+overlamer@users.noreply.github.com>
2020-06-09 01:58:39 +0300

    Add files via upload

---
+++ requirement.sh
@@ -0,0 +1,23 @@
+
+#!/bin/bash
+
+clear
+apt update
+apt upgrade
+apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
+apt install nano -y
+apt install git -y
+apt install zsh -y
+apt install ruby -y
+apt install curl -y
+gem install lolcat
+clear
+figlet -f big Done !!! | lolcat
+echo
+echo -e "\e[1m Now Run \e[32mbash banner\e[0m...!!!"
+echo
+echo -e "\e[1m\e[32m Developed by :\e[33m @overlamer2130"
+echo
+echo
